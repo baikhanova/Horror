3c6339a3da5aa8b490bc1fcce31ff85a2eaf72dd
baikhanova <51983446+baikhanova@users.noreply.github.com>
2020-09-29 16:45:20 +0600

    New Project

---
+++ Assets/Scripts/FlashLight.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlashLight : MonoBehaviour
+{
+
+	Light spotLight;
+
+	public float maxBatteryLife = 100;
+	public float curBatteryLife;
+	float lightDrain = 1f;
+
+	public Image batteryUI;
+
+	void Start()
+	{
+		spotLight = GetComponent<Light>();
+		curBatteryLife = maxBatteryLife;
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.F))
+		{
+			if (spotLight.enabled == false)
+			{
+				spotLight.enabled = true;
+			}
+			else
+			{
+				spotLight.enabled = false;
+			}
+		}
+
+		if(spotLight.enabled == true && curBatteryLife > 0)
+		{
+			curBatteryLife -= lightDrain * Time.deltaTime;
+		}
+
+		if(curBatteryLife <=0)
+		{
+			curBatteryLife = 0;
+			spotLight.enabled = false;
+		}
+
+		batteryUI.transform.localScale = new Vector3(
+			curBatteryLife / maxBatteryLife,
+			batteryUI.transform.localScale.y,
+			batteryUI.transform.localScale.z
+			);
+
+	}
+}
